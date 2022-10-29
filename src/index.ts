fcb9305aa3d4f500cd71b8979f1835e5cd7f635a
Leo Holman <lah2719@g.rit.edu>
2022-10-28 20:54:03 -0400

    WIP: creating first post request

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import fastify from 'fastify';
+import { REPL_MODE_SLOPPY } from 'repl';
+
+interface IBody {
+    seed?: string,
+    length?: number,
+    locale?: string,
+};
+
+interface IReply {
+    word: string,
+    seed: string,
+};
 
 const server = fastify();
-server.get("/ping", async (request, reply) => {
-    console.log(request, reply);
+server.get("/ping", async (_request, _reply) => {
     return "pong\n";
+});
+
+server.post<{ Body: IBody, Reply: IReply }>("/generate", async (request, reply) => {
+    const { seed = NaN, length = NaN, locale = 'en' } = request.body;
+    if (locale !== 'en') {
+        reply.code(400).send({ 'msg': 'Local not supported' })
+    }
+
 });
 
 server.listen({ port: 3000 }, (err, address) => {
